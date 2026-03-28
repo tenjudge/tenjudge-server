6068a45bb16267ef25f031a49d63873d96531741
Xiao Yushi <yushi.xiao@qq.com>
2026-03-28 22:38:09 +0800

    init

---
+++ src/test/java/io/github/yush1x/tenjudge/server/TenjudgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yush1x.tenjudge.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenjudgeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
